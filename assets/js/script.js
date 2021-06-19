6610ecaa6d46654d91d502d9cff858b853673b61
philip howley <howley.phil@gmail.com>
2021-06-19 11:55:08 -0400

    Add JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,17 @@
 //   myTextEl.textContent = myBtnEl.getAttribute('data-whatever');
 // }
 
-// myBtnEl.addEventListener('click', swapSomeText);
-$(document).ready(function () {
-   $(function () {
-     $('[data-toggle="tooltip"]').tooltip();
-   });
-});
+// // myBtnEl.addEventListener('click', swapSomeText);
+// $(document).ready(function () {
+//    $(function () {
+//      $('[data-toggle="tooltip"]').tooltip();
+//    });
+// });
+const roll = 5;
+if (roll === 1 || roll === 6) {
+  console.log('You win!');
+} else if (roll <= 3) {
+  console.log('Roll again!');
+} else {
+  console.log('looks like both of the above conditions were false')
+}
